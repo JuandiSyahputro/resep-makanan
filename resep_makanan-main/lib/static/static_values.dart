9545367a23772fc0d99f9c7bde28e397c4dfdf6a
juandi <juandi.syahputra.tsm1@gmail.com>
2022-07-05 08:14:33 +0700

    Upload tugas resep makanan

---
+++ resep_makanan-main/lib/static/static_values.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kApiURLDomain = 'api.spoonacular.com';
+const kApiEndPointRecipes = 'recipes/complexSearch';
+const kApiEndPointRecipe = 'recipes';
+const kApiKey = '9d8abb20612246f5b8e21fb3e640116e';
+
+const kPrimaryColor = Colors.redAccent;
